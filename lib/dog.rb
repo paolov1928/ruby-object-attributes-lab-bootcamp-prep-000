ccf1c9219b622b7858ea6fdfe9ef656439350220
Paolo Ventura <ventura.paolo@gmail.com>
2018-09-20 14:43:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,7 @@
-## `Dog` and `lib/dog.rb`
-
 ### Give a Dog a Name
-
 #You'll be teaching `Dog` about their names through two methods, `#name`, and `#name=` that read and write to a corresponding instance variable `@name`.
-
 ### Give a Dog a Breed
-
 #You'll be teaching `Dog` about their breed through two methods, `#breed`, and `#breed=` that read and write to a corresponding instance variable `@breed`.
 
-## `Person` and `lib/person.rb`
+class Dog
+  
